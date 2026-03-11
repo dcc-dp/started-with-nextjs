488f765c33c7d455ce00e938c91d33a7ecfd50fc
Safira Muztasyifa Syah <safira.syah86@gmail.com>
2026-03-11 22:26:15 +0800

    asdfjkl

--- src/app/produk/[...slug]/page.js
+++ src/app/produk/[...slug]/page.js
@@ -1,5 +1,5 @@
 async function getData() {
-  const res = await fetch("https://fakestoreapi.com/productss");
+  const res = await fetch("https://fakestoreapi.com/products");
 
   if (!res.ok) {
     throw new Error("Failed to fetch data");
